cfe90f528477ceea545d2cc60f5acabb098072af
Dawn Carroll <dsc254@gmail.com>
2018-01-24 11:06:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,15 @@
 var playlist = {
+<<<<<<< HEAD
     benjaminGibbard: "You Remind Me Of Home",
     gesuNoKiwamiOtome: "Darumasan", 
     simonAndGarfunkel: "El Condor Pasa",
     theBeautifulSouth: "Good As Gold"
+=======
+    "Benjamin Gibbard": "You Remind Me Of Home",
+    GesuNoKiwamiOtome: "Darumasan", 
+    SimonAndGarfunkel: "El Condor Pasa",
+    TheBeautifulSouth: "Good As Gold"
+>>>>>>> dbcc7a500c0acd234247ae3fa90d118e90ae08da
 };
 
 
@@ -13,10 +20,14 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 
+<<<<<<< HEAD
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist];
   
   return playlist;
 }
+=======
+
+>>>>>>> dbcc7a500c0acd234247ae3fa90d118e90ae08da
 
 
